f38e8e751cf7946063a252a488672fd1cb8d1bfa
maemon <47819358+maemon4095@users.noreply.github.com>
2022-09-01 23:20:41 +0900

    bundler

--- tools/adjacent-color-checker/src/main.rs
+++ tools/adjacent-color-checker/src/main.rs
@@ -1,3 +1,20 @@
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let app = web_sys::window()
+        .expect_throw("window was not found.")
+        .document()
+        .expect_throw("document was not found in window.")
+        .get_element_by_id("#app")
+        .expect_throw("app was not found in document.");
+
+    yew::start_app_in_element::<App>(app);
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <a>{"yo!"}</a>
+    }
 }
